538a4259deddfd7285339c3270934885e3d746d4
anish <hsinatnias@gmail.com>
2023-10-16 23:55:53 +0530

    Datampper primary complete

--- src/Semiframe/LiquidOrm/DataMapper/DataMapperFactory.php
+++ src/Semiframe/LiquidOrm/DataMapper/DataMapperFactory.php
@@ -1,1 +1,26 @@
 <?php
+
+declare(strict_types=1);
+
+namespace Semiframe\DataMapper;
+
+use Semiframe\DataMapper\Exception\DataMapperException;
+
+class DataMapperFactory
+{
+
+    public function __construct()
+    {
+    }
+
+    public function create(string $databaseConnectionString, string $dataMapperEnvironmentConfiguration): DataMapperInterface
+    {
+        $credentials = (new $dataMapperEnvironmentConfiguration([]))->getDatabaseCredentials('mysql');
+
+        $databaseConnectionObject = new $databaseConnectionString($credentials);
+        if(!$databaseConnectionObject instanceof DataMapperInterface){
+            throw new DataMapperException($databaseConnectionString .'is not a valid database connection object');
+        }
+        return  new DataMapper($databaseConnectionObject);
+    }
+}
--- src/Semiframe/LiquidOrm/DataMapper/DatamapperEnvironmentConfiguration.php
+++ src/Semiframe/LiquidOrm/DataMapper/DatamapperEnvironmentConfiguration.php
@@ -1,1 +1,62 @@
 <?php
+declare(strict_types=1);
+
+namespace Semiframe\DataMapper;
+
+use Semiframe\DataMapper\Exception\DataMapperException;
+use Semiframe\DataMapper\Exception\DataMapperInvalidArgumentException;
+
+class DataMapperEnvironmentConfiguration
+{
+    /**
+     * @var array
+     */
+    private array $credentials =[];
+
+    /**
+     * Constructor of the class
+     * @param array $credentials
+     * @return void
+     */
+    public function __construct(array $credentials)
+    {
+        $this->credentials= $credentials;
+    }
+
+
+    /**
+     * This function helps to select the database credentials based on the driver or developer chooses
+     * @param string $driver
+     * @return array
+     */
+    public function getDatabaseCredentials(string $driver): array
+    {
+        $connectionArray =[];
+
+        foreach ($this->credentials as $credential){
+            if(array_key_exists($driver, $credential)){
+                $connectionArray = $credential[$driver];
+            }
+        }
+        return $connectionArray;
+    }
+
+    /** Function checks the credentials for validity
+     * @param string $driver
+     * @return void
+     */
+    private  function  isCredentialsValid(string $driver): void
+    {
+        if(empty($driver) && !is_string($driver)){
+            throw new DataMapperInvalidArgumentException('Invalid argument. Invalid driver provided');
+        }
+        if(!is_array($this->credentials)){
+            throw new DataMapperInvalidArgumentException('Invalid Credentials');
+        }
+        if(!in_array($driver, array_keys($this->credentials[$driver]))){
+            throw new DataMapperInvalidArgumentException('Invalid or this database driver is not supported');
+        }
+
+    }
+
+}
